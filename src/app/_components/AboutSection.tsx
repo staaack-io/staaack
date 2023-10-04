77239c1142bfb570c198dcf89fd5a21ec3732af9
Alexis Détrie <detrie.alexis@gmail.com>
2023-10-04 19:50:32 +0200

    WIP on the about section - responsive

--- src/app/_components/AboutSection.tsx
+++ src/app/_components/AboutSection.tsx
@@ -6,59 +6,59 @@ import SimpleCardTextContent from "@/app/_components/button/SimpleCardTextConten
 
 const ServiceSection = (props) => {
     return <section {...props}>
-            <div className="content-center grid grid-cols-6 gap-10">
-                <div className="col-span-6 text-center">About</div>
-                <div className="col-span-2 row-span-2 row-start-2">
-                    <SimpleCard className="">
-                        <img src="https://placehold.co/500x600" height={500} width={400} alt="Photo Alexis Détrie" />
-                    </SimpleCard>
-                </div>
-                <div className="col-start-1 row-start-4">
-                    <SimpleCard>
-                        <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
-                    </SimpleCard>
-                </div>
-                <div className="col-start-2 row-start-4">
-                    <SimpleCard>
-                        <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
-                    </SimpleCard>
-                </div>
-                <div className="col-start-1 row-start-5">
-                    <SimpleCard>
-                        <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
-                    </SimpleCard>
-                </div>
-                <div className="col-start-2 row-start-5">
-                    <SimpleCard>
-                        <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
-                    </SimpleCard>
-                </div>
-                <div className="col-span-2 row-span-3 col-start-3 row-start-2">
-                    <SimpleCard>
-                        <SimpleCardTextContent></SimpleCardTextContent>
-                    </SimpleCard>
-                </div>
-                <div className="col-span-2 col-start-3 row-start-5">
-                    <SimpleCard>6</SimpleCard>
-                </div>
-                <div className="col-start-5 row-start-2">
-                    <SimpleCard>7</SimpleCard>
-                </div>
-                <div className="col-start-6 row-start-2">
-                    <SimpleCard>8</SimpleCard>
-                </div>
-                <div className="col-span-2 col-start-5 row-start-3">
-                    <SimpleCard>9</SimpleCard>
-                </div>
-                <div className="col-start-5 row-start-4">
-                    <SimpleCard>10</SimpleCard>
-                </div>
-                <div className="col-start-6 row-start-4">
-                    <SimpleCard>11</SimpleCard>
-                </div>
-                <div className="col-span-2 col-start-5 row-start-5">
-                    <SimpleCard>12</SimpleCard>
-                </div>
+        <div className="text-center font-bold text-3xl text-white">About</div>
+        <div className="content-center grid grid-cols-2 gap-4 lg:grid-cols-6 lg:gap-10">
+            <div className="col-span-2 row-span-2 row-start-1">
+                <SimpleCard className="">
+                    <img src="https://placehold.co/500x600" height={500} width={400} alt="Photo Alexis Détrie"/>
+                </SimpleCard>
+            </div>
+            <div className="lg:col-start-1 lg:row-start-3">
+                <SimpleCard>
+                    <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
+                </SimpleCard>
+            </div>
+            <div className="lg:col-start-2 lg:row-start-3">
+                <SimpleCard>
+                    <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
+                </SimpleCard>
+            </div>
+            <div className="lg:col-start-1 lg:row-start-4">
+                <SimpleCard>
+                    <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
+                </SimpleCard>
+            </div>
+            <div className="lg:col-start-2 lg:row-start-4">
+                <SimpleCard>
+                    <SimpleCard1Content imgSrc="./cpu.svg" text1="12" text2="ans d'XP"/>
+                </SimpleCard>
+            </div>
+            <div className="col-span-2 lg:row-span-3 lg:col-start-3 lg:row-start-1">
+                <SimpleCard>
+                    <SimpleCardTextContent></SimpleCardTextContent>
+                </SimpleCard>
+            </div>
+            <div className="col-span-2 lg:col-start-3 lg:row-start-4">
+                <SimpleCard>6</SimpleCard>
+            </div>
+            <div className="lg:col-start-5 lg:row-start-1">
+                <SimpleCard>7</SimpleCard>
+            </div>
+            <div className="lg:col-start-6 lg:row-start-1">
+                <SimpleCard>8</SimpleCard>
+            </div>
+            <div className="col-span-2 lg:col-start-5 lg:row-start-2">
+                <SimpleCard>9</SimpleCard>
+            </div>
+            <div className="lg:col-start-5 lg:row-start-3">
+                <SimpleCard>10</SimpleCard>
+            </div>
+            <div className="lg:col-start-6 lg:row-start-3">
+                <SimpleCard>11</SimpleCard>
+            </div>
+            <div className="col-span-2 lg:col-start-5 lg:row-start-4">
+                <SimpleCard>12</SimpleCard>
+            </div>
         </div>
     </section>;
 }
