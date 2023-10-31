c5cf332b81f0bba619a86fe67484298a065a5a6f
Alexis Détrie <detrie.alexis@gmail.com>
2023-10-31 19:59:53 +0100

    WIP Change map in order to build the solution

--- src/app/_components/About/MapCardContent.tsx
+++ src/app/_components/About/MapCardContent.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-// import OpenStreetMap from "@/app/_components/Common/map/OpenStreetMap";
-const DEFAULT_CENTER = [49.5985502, 6.1263061]
-const ZOOM = 17
 import dynamic from 'next/dynamic'
-// import OpenStreetMap from '../component/OpenStreetMap'
+
 const OpenStreetMap = dynamic(() => import('../Common/map/OpenStreetMap'), {
     ssr: false,
 })
@@ -11,27 +8,6 @@ const MapCardContent = (props: any) => {
 
     return <div className="text-4xl justify-center bg-transparent rounded-3xl shadow-2xl h-72 md:h-full w-full">
         <OpenStreetMap />
-        {/* eslint-disable-next-line react/jsx-no-undef */}
-        {/*<Map className="w-full h-full rounded-3xl shadow-2xl" width="800" height="400" center={DEFAULT_CENTER}*/}
-        {/*       zoom={ZOOM}>*/}
-        {/*    {// @ts-ignore*/}
-        {/*        ({TileLayer, Marker, Popup}) => (*/}
-        {/*            <>*/}
-        {/*                <TileLayer*/}
-        {/*                    url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"*/}
-        {/*                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"*/}
-        {/*                />*/}
-        {/*                <Marker position={DEFAULT_CENTER}>*/}
-        {/*                    <Popup>*/}
-        {/*                        <div className="text-xl">*/}
-        {/*                            20 rue de Hollerich <br/>*/}
-        {/*                            1740 Luxembourg <br/>*/}
-        {/*                        </div>*/}
-        {/*                    </Popup>*/}
-        {/*                </Marker>*/}
-        {/*            </>*/}
-        {/*        )}*/}
-        {/*</Map>*/}
     </div>;
 }
 
--- src/app/_components/Common/map/OpenStreetMap.js
+++ src/app/_components/Common/map/OpenStreetMap.js
@@ -1,28 +1,43 @@
-import React, {useState, useRef} from 'react'
-import {MapContainer, TileLayer, Marker} from 'react-leaflet'
+import React, {useEffect, useRef} from 'react'
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import Leaflet from "leaflet";
 
 const OpenStreetMap = () => {
-    const [center, setCenter] = useState({lat: -4.043477, lng: 39.668205})
-    const ZOOM_LEVEL = 9
+    const DEFAULT_CENTER = [49.5985502, 6.1263061]
+    const ZOOM = 17
     const mapRef = useRef()
 
-    return <div className="w-full h-full rounded-3xl shadow-2xl z-50" >
-                    <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef} width="800" height="400">
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                        />
-                        {location.loaded && !location.error && (
-                            <Marker
-                                position={[
-                                    location.coordinates.lat,
-                                    location.coordinates.lng,
-                                ]}
-                            ></Marker>
-                        )}
-                    </MapContainer>
+    useEffect(() => {
+        (async function init() {
+            delete Leaflet.Icon.Default.prototype._getIconUrl;
+            Leaflet.Icon.Default.mergeOptions({
+                iconRetinaUrl: './img/logo.svg',
+                iconUrl: './img/logo.svg',
+                iconSize: [50, 50],
+                iconAnchor: [25, -5],
+                popupAnchor: [0, 0],
+                tooltipAnchor: [0, 0],
+                shadowSize: [0, 0]
+            });
+        })();
+    }, []);
+
+    return <MapContainer center={DEFAULT_CENTER} zoom={ZOOM} ref={mapRef}>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;https://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        <Marker position={DEFAULT_CENTER}>
+            <Popup>
+                <div className="text-xl">
+                    20 rue de Hollerich <br/>
+                    1740 Luxembourg <br/>
                 </div>
+            </Popup>
+        </Marker>
+    </MapContainer>
 }
 
 export default OpenStreetMap
