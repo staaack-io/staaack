d5c1e81d0a9bcade18d9774ba10812e695544fc1
Alexis Détrie <detrie.alexis@gmail.com>
2023-11-14 18:15:48 +0100

    Home section

--- src/app/_components/Home/HomeSection.tsx
+++ src/app/_components/Home/HomeSection.tsx
@@ -5,9 +5,8 @@ import ButtonStylised from "@/app/_components/Home/ButtonStylised";
 import Spline from "@splinetool/react-spline";
 
 const HomeSection = (props: any) => {
-    return <section className="p-8 pb-0 h-screen">
-        <div className="flex flex-col lg:flex-row pt-24 h-[calc(100%-3em)] items-center">
-            <div className="md:ml-8 order-2 lg:order-1 lg:w-1/2 mb-24">
+    return <section className="p-8 pb-0 min-h-screen flex flex-col lg:flex-row  items-center">
+            <div className="md:ml-8 order-2 lg:order-1 lg:w-1/2 mb-24 h-full">
                 <div className="pb-36">
                     <h1 className="absolute font-shadow-logo font-logo font-extrabold text-5xl md:text-6xl text-transparent inline-block bg-clip-text">staaack</h1>
                     <h1 className="absolute font-color-logo font-logo font-extrabold text-5xl md:text-6xl text-transparent inline-block bg-clip-text">staaack</h1>
@@ -21,11 +20,9 @@ const HomeSection = (props: any) => {
                     <ButtonStylised text="Download my resume"/>
                 </div>
             </div>
-            <div className="order-1 lg:order-2 lg:w-1/2 flex items-center justify-center h-full">
-
+            <div className="order-1 lg:order-2 lg:w-1/2 flex items-center justify-center h-screen lg:h-full">
                 <Spline scene="https://prod.spline.design/ttPadaMLh4iul2yS/scene.splinecode" />
             </div>
-        </div>
     </section>;
 }
 
