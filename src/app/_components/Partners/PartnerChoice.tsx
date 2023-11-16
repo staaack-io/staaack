082c6b20926e06de550e6afd365cefac4af7d85b
Alexis Détrie <detrie.alexis@gmail.com>
2023-11-16 16:21:47 +0100

    Update Partner choice

--- src/app/_components/Partners/PartnerChoice.tsx
+++ src/app/_components/Partners/PartnerChoice.tsx
@@ -6,7 +6,6 @@ const PartnerChoice = (props: { name: any }) => {
     const [checked, setChecked] = useState(false);
 
     useEffect(() => {
-        console.log(checked)
     }, []);
 
     const handleChange = () => {
@@ -14,24 +13,30 @@ const PartnerChoice = (props: { name: any }) => {
         setChecked(!checked);
     };
 
+    const onClick = (category: any) => {
+        props.name(category);
+        setChecked(false);
+        setName(category)
+    };
+
     return <div className="flex w-full p-8 justify-center items-center z-50 ">
         <div className="dropdown">
             <input type="checkbox" id="dropdown" checked={checked} onChange={handleChange}/>
 
                 <label className="dropdown__face" htmlFor="dropdown">
-                    <div className="dropdown__text">All</div>
+                    <div className="dropdown__text">{name}</div>
 
                     <div className="dropdown__arrow"></div>
                 </label>
 
                 <ul className="dropdown__items">
-                    <li className="p-5" onClick={() => {props.name("All"); setChecked(false)}}>All</li>
-                    <li className="p-5" onClick={() => {props.name("Big four"); setChecked(false)}}>Big four</li>
-                    <li className="p-5" onClick={() => {props.name("Administrations"); setChecked(false)}}>Administrations</li>
-                    <li className="p-5" onClick={() => {props.name("IT services companies"); setChecked(false)}}>IT services companies</li>
-                    <li className="p-5" onClick={() => {props.name("Startup"); setChecked(false)}}>Startup</li>
-                    <li className="p-5" onClick={() => {props.name("Banks"); setChecked(false)}}>Banks</li>
-                    <li className="p-5" onClick={() => {props.name("Others"); setChecked(false)}}>Other</li>
+                    <li className="p-5" onClick={() => onClick("All")}>All</li>
+                    <li className="p-5" onClick={() => onClick("Big four")}>Big four</li>
+                    <li className="p-5" onClick={() => onClick("Administrations")}>Administrations</li>
+                    <li className="p-5" onClick={() => onClick("IT services companies")}>IT services companies</li>
+                    <li className="p-5" onClick={() => onClick("Startup")}>Startup</li>
+                    <li className="p-5" onClick={() => onClick("Banks")}>Banks</li>
+                    <li className="p-5" onClick={() => onClick("Others")}>Other</li>
                 </ul>
         </div>
 
