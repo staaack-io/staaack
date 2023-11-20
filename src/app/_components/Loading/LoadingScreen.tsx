725875a311aec1462c09355427df53379f64bfdf
Alexis Détrie <detrie.alexis@gmail.com>
2023-11-20 11:22:51 +0100

    Remove react-spring + clean code

--- src/app/_components/Loading/LoadingScreen.tsx
+++ src/app/_components/Loading/LoadingScreen.tsx
@@ -3,36 +3,7 @@
 import Image from "next/image";
 import {motion, useAnimate} from "framer-motion";
 import React, {useState} from "react";
-import {useTrail, a} from '@react-spring/web'
 
-const Trail = (props: any) => {
-    let lastFrame = -1;
-    const items = React.Children.toArray(props.children)
-    const [open, set] = useState(true)
-    const trail = useTrail(items.length, {
-        config: {mass: 5, tension: 2000, friction: 200},
-        opacity: open ? 1 : 0,
-        x: open ? 0 : 20,
-        height: open ? 110 : 0,
-        from: {opacity: 0, x: 20, height: 0},
-        onStart: result => {
-        },
-        onChange: result => {
-            if (result.value.opacity == 1) {
-                props.onFinish()
-            }
-        }
-    })
-    return (
-        <div>
-            {trail.map(({height, ...style}, index) => (
-                <a.div key={index} className="trailsText" style={style}>
-                    <a.div style={{height}}>{items[index]}</a.div>
-                </a.div>
-            ))}
-        </div>
-    )
-}
 
 const LoadingScreen = (props: any) => {
     const [endAnimation, setEndAnimmation] = useState(false)
@@ -53,11 +24,7 @@ const LoadingScreen = (props: any) => {
                 </motion.div>
 
                 <div>
-                    <Trail open={open} onFinish={() => {
-                        props.outAnim();
-                    }}>
-                        <h1 id="loader-text" className="font-logo font-extrabold text-5xl md:text-6xl text-black">staaack</h1>
-                    </Trail>
+                    <h1 id="loader-text" className="font-logo font-extrabold text-5xl md:text-6xl text-black">staaack</h1>
                 </div>
             </div>
         </div>);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,62 +7,33 @@ import PartnersSection from "@/app/_components/Partners/PartnersSection";
 import Navbar from "@/app/_components/Navbar/Navbar";
 import AboutSection from "@/app/_components/About/AboutSection";
 import Footer from "@/app/_components/Footer/Footer";
-import {
-    animate,
-} from 'framer-motion';
 import LoadingScreen from "@/app/_components/Loading/LoadingScreen";
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
     const [animationIsFinished, setAnimationIsFinished] = useState(false)
-    const [hideAnimation, setHideAnimation] = useState(false)
     const [splineIsLoaded, setSplineIsLoaded] = useState(false)
 
     useEffect(() => {
         if (animationIsFinished && splineIsLoaded) {
-            //setHideAnimation(true)
-            let animation = animate("#loader-text", {opacity: 0, y: -100}, {ease: "linear"})
-                .then(() => {
-                    animate("#loader-image", {opacity: 0, y: 100}, {ease: "linear"})
-                        .then(() => {
-                            animate("#loader", {
-                                opacity: 1,
-                                height: 0,
-                                width: 0,
-                                borderRadius: "100%",
-                            }, {ease: "easeIn"})
-                                .then(() => {
-                                    animate("#loader", {
-                                        opacity: 1,
-                                        height: 0,
-                                        width: 0,
-                                        borderRadius: "100%",
-                                    }, {ease: "easeIn"})
-                                })
-                        })
-                })
+
         }
     }, [animationIsFinished, splineIsLoaded]);
     return (
         <>
-            <div>
-                <LoadingScreen outAnim={() => {
-                    setAnimationIsFinished(true)
-
-                }} hideAnimation={hideAnimation}/>
-            </div>
-            <>
-                <Navbar showAnim={splineIsLoaded}/>
-                <HomeSection onSlineAppLoad={() => {
-                    setSplineIsLoaded(true)
-                }} showAnim={splineIsLoaded}/>
-                <AboutSection/>
-                <ServicesSection/>
-                <SkillsSection/>
-                <PartnersSection/>
-                <ContactSection/>
-                <Footer/>
-            </>
+            <LoadingScreen outAnim={() => {
+                setAnimationIsFinished(true)
+            }} />
+            <Navbar showAnim={splineIsLoaded}/>
+            <HomeSection onSlineAppLoad={() => {
+                setSplineIsLoaded(true)
+            }}/>
+            <AboutSection/>
+            <ServicesSection/>
+            <SkillsSection/>
+            <PartnersSection/>
+            <ContactSection/>
+            <Footer/>
         </>
     )
 }
