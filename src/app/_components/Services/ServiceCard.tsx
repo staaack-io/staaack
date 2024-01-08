4e650d6fb0c6a771fd0e02b3154a063c533f00b2
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-08 11:11:42 +0100

    update styles

--- src/app/_components/Services/ServiceCard.tsx
+++ src/app/_components/Services/ServiceCard.tsx
@@ -3,18 +3,20 @@ import React from "react";
 
 const ServiceCard = (props: any) => {
     return <div className={"my-10 ml-4 bg-white rounded-2xl p-10 " +
-        " w-48 h-72 sm:w-96 sm:h-[32rem]" +
-        " flex flex-col " +
+        " w-[90%] min-h-72 sm:w-96 sm:h-[32rem]" +
+        " flex flex-col justify-items items-center" +
         " hover:drop-shadow-lg hover:transform-gpu duration-500 p-10" + props.className}>
         <div className="flex justify-center ">
-            <Image className="pic-services-color" src={props.icon} alt={props.serviceName} width={100}
-                   height={100}/></div>
+            <div className="w-1/2 h-1/2 sm:w-full sm:h-full">
+                <Image className="pic-services-color" src={props.icon} alt={props.serviceName} width={100}
+                       height={100}/></div>
+            </div>
         <div
-            className="text-3xl md:text-4xl text-center font-bold text-transparent
+            className="text-4xl sm:text-3xl md:text-4xl sm:text-center font-bold text-transparent
                      bg-gradient-to-r from-pink-600 via-purple-700 to-blue-400
                       inline-block bg-clip-text">{props.serviceName}
         </div>
-        <div className="pt-8 w-full">
+        <div className="col-span-2 sm:col-span-0 pt-8 w-full">
             {props.children}
         </div>
     </div>
