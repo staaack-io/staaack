2b2186863c174fabf9ca12c3727d9a4814e32005
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-08 12:08:45 +0100

    update partner card style

--- src/app/_components/Partners/PartnerCard.tsx
+++ src/app/_components/Partners/PartnerCard.tsx
@@ -6,9 +6,9 @@ const PartnerCard = (props: any) => {
     return <>
         {(props.selectedSectionPartner == props.category || props.selectedSectionPartner == "All") &&
             <div className="flex flex-col justify-center items-center p-5">
-                <div className="partner-card-parent h-full grayscale hover:grayscale-0 hover:scale-110">
+                <div className="h-full grayscale hover:grayscale-0 hover:drop-shadow-lg hover:transform-gpu duration-500">
                     <div
-                        className="partner-card-child h-full p-8 bg-gray-100 hover:bg-white flex justify-center items-center">
+                        className="h-full p-8 bg-gray-100 hover:bg-white flex justify-center items-center rounded-2xl">
                         <Image src={"/img/logo/" + props.image} alt="" height={150} width={150} className=""></Image>
                     </div>
                 </div>
