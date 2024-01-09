6629192196bfe056e85b1480b23b309facc8bc75
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-09 20:33:19 +0100

    clean code

--- src/app/_components/Common/Card1x1.tsx
+++ src/app/_components/Common/Card1x1.tsx
@@ -2,7 +2,7 @@ const Card1x1 = (props: any) => {
     return <div className={
         " bg-white rounded-3xl p-5" +
         " flex flex-col items-center justify-center" +
-        " aspect-square w-[full] sm:h-[265px] sm:w-[265px] md:h-[215px] md:w-[215px] lg:h-[190px] lg:w-[190px] xl:h-[210px] xl:w-[210px] 2xl:h-[190px] 2xl:w-[190px]" +
+        " aspect-square w-full sm:h-[265px] sm:w-[265px] md:h-[215px] md:w-[215px] lg:h-[190px] lg:w-[190px] xl:h-[210px] xl:w-[210px] 2xl:h-[190px] 2xl:w-[190px]" +
         " hover:drop-shadow-lg hover:transform-gpu duration-500 " +
         props.className
     }>
--- src/app/_components/Common/CardSchedule.tsx
+++ src/app/_components/Common/CardSchedule.tsx
@@ -6,7 +6,7 @@ const CardSchedule = (props: any) => {
     return <div className={props.className +
         " bg-white rounded-3xl border-[#54A4FF] border-2" +
         " flex flex-col items-center justify-center" +
-        " aspect-square w-[full] sm:h-[265px] sm:w-[265px] md:h-[215px] md:w-[215px] lg:h-[190px] lg:w-[190px] xl:h-[210px] xl:w-[210px] 2xl:h-[190px] 2xl:w-[190px]" +
+        " aspect-square w-full sm:h-[265px] sm:w-[265px] md:h-[215px] md:w-[215px] lg:h-[190px] lg:w-[190px] xl:h-[210px] xl:w-[210px] 2xl:h-[190px] 2xl:w-[190px]" +
         " overflow-hidden" +
         " hover:drop-shadow-lg hover:transform-gpu duration-500"
     }>
