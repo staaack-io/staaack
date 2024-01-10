c479d017b7cc1f6f7b14bf7c862e96dbac97017d
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-10 14:57:05 +0100

    Clean

--- src/app/_components/Navbar/Navbar.tsx
+++ src/app/_components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client"
 import Image from "next/image";
-import a from "next/a";
 import ButtonStylised from "@/app/_components/Home/ButtonStylised";
 import {useEffect, useState} from "react";
 import {motion, useAnimation} from "framer-motion";
