8a7c4d2f047c008d6925f447845d2b208bde467f
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-24 23:04:10 +0100

    Change memoji

--- src/app/components/Common/CardMe.tsx
+++ src/app/components/Common/CardMe.tsx
@@ -13,13 +13,11 @@ const CardMe = (props: any) => {
       }
     >
       <div className='flex h-full w-full flex-col gap-2 overflow-x-auto rounded-3xl'>
-        <Image
-          src='/img/emoj.gif'
-          alt=''
-          height={200}
-          width={200}
-          className=''
-        />
+        <div className="">
+          <video autoPlay muted playsInline className="rounded-full border-2 border-[#29d9ff50] w-36 h-36 ">
+            <source src="/video/memoji.mp4" type="video/mp4" />
+          </video>
+        </div>
         {/*<p className="p-3">*/}
         {/*    Bonjour ! Je suis Alexis, <b>freelance en développement et architecture logicielle</b>, basé au*/}
         {/*    Luxembourg.*/}
