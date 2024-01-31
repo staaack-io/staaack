ab1cce77a63dad958383c94a94513aae7a462120
Alexis Détrie <detrie.alexis@gmail.com>
2024-01-31 19:42:22 +0100

    analytics

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import Navbar from '@/app/components/Navbar/Navbar';
 import HomeSection from '@/app/components/Home/HomeSection';
 import ServicesSection from '@/app/components/Services/ServicesSection';
@@ -7,6 +7,7 @@ import SkillsSection from '@/app/components/Skills/SkillsSection';
 import PartnersSection from '@/app/components/Partners/PartnersSection';
 import ContactSection from '@/app/components/Contact/ContactSection';
 import Footer from '@/app/components/Footer/Footer';
+import { UsermavenAnalytics } from '@/app/components/Common/usermavenAnalytics';
 
 type HomeProps = {
   params: { locale: string };
@@ -15,6 +16,9 @@ export default function Home({ params: { locale } }: HomeProps) {
 
   return (
     <div className='flex flex-col bg-[#EDEEF0]'>
+    <Suspense fallback={null}>
+      <UsermavenAnalytics />
+    </Suspense>
       <div className='flex flex-row'>
         <div className="w-full">
           <Navbar />
---
+++ src/app/components/Common/usermavenAnalytics.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useEffect } from "react"
+import { usePathname, useSearchParams } from "next/navigation"
+import { UsermavenClient, usermavenClient } from "@usermaven/sdk-js"
+
+export function UsermavenAnalytics() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const url = `${pathname}?${searchParams}`
+    const usermaven: UsermavenClient = usermavenClient({
+      key: "UMrOnO6zZz",
+      tracking_host: "https://events.usermaven.com"
+    })
+
+    // Track page views called on each page load and on each route change
+    usermaven.track("pageview")
+    console.log("Usermaven pageview event sent for url: " + url)
+  }, [pathname, searchParams])
+
+  return null
+}
